f53d8a2a568f1ce86aa2952973e0d2a659b833c5
Gustavo <gcastro@novopayment.com>
2020-01-08 09:19:52 -0500

    primer commit

---
+++ src/main/java/com/udemy/app/ReactivaApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/app/ReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
